ead7ac4d0ae2524c4fbe939a93c8c1e9678df4d3
Ryan Maskell <ryan@savorweb.com>
2014-05-05 16:54:11 -0700

    base

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include("_/inc/head.php"); ?>
+
+<div class="row">
+	<div class="colspan12-7 colspan6-3 padded colspan2-2 as-grid">
+		<div id='slider' class='swipe'>
+		  <div id="slider-pictures" class='swipe-wrap editable'>
+		    <div class="repeatable"><img src="http://www.placehold.it/500x500" alt=""></div>
+		    <div class="repeatable"><img src="http://www.placehold.it/500x500" alt=""></div>
+		    <div class="repeatable"><img src="http://www.placehold.it/500x500" alt=""></div>
+		  </div>
+		</div>
+	</div>
+	<div class="colspan12-5 colspan6-3 colspan2-2 as-grid padded typeset">
+		<section class="border-b">
+			<h1>Quality Commercial and Residential Plumbing Since 1980</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, deleniti, quos, nisi numquam voluptatum inventore quibusdam quis voluptas accusantium aliquam sed laboriosam. Nulla, enim, nisi aliquam eaque mollitia iusto ut.</p>
+		</section>
+		<section class="border-b">
+			<h1>What's New</h1>
+			<small>03/21/14</small>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, deleniti, quos, nisi numquam voluptatum inventore quibusdam quis voluptas accusantium aliquam sed laboriosam. Nulla, enim, nisi aliquam eaque mollitia iusto ut.</p>
+			<a href="#">View All News</a>
+		</section>
+		<section class="border-b">
+			<h1>Client Testimonials</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, deleniti, quos, nisi numquam voluptatum inventore quibusdam quis voluptas accusantium aliquam sed laboriosam. Nulla, enim, nisi aliquam eaque mollitia iusto ut.</p>
+			<a href="#">View All Testimonials</a>
+		</section>
+	</div>
+</div>
+
+<?php include("_/inc/foot.php"); ?>
